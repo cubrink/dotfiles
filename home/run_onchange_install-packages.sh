f88dd7ff9a63cd873d53e19d3e18f955693745e7
Curtis Brinker <curtisbrnkr@gmail.com>
2025-06-14 12:42:10 -0500

    update install script

--- home/run_onchange_install-packages.sh
+++ home/run_onchange_install-packages.sh
@@ -1,2 +1,41 @@
 #!/bin/sh
-sudo apt install build-essential
+sudo apt install -y \
+    build-essential \
+    make \
+    git \ 
+    # cargo \
+    curl \
+    vim \
+    htop \
+    wget \
+    tmux \
+    cmake \
+    sudo \
+    snap \
+    flatpak \
+    unzip \
+    unrar \
+    7zip \
+    ffmpeg \
+    rsync \
+    ca-certificates \
+    gnupg \
+    tree \
+    jq \
+    # tomlq \
+    # yq \
+    vlc \
+    podman \
+    imagemagick \
+    pandoc \
+    golang \
+	# ruby for gem
+    ruby \
+    npm \
+    graphviz \
+    httpie \
+    gdb \
+    valgrind \
+    ccmake \
+    cron \
+    crontab \
