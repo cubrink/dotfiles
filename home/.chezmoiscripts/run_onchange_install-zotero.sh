ac9401a9ef9e66c2d08b200a83411fe5deb40da3
Curtis Brinker <curtisbrnkr@gmail.com>
2025-08-21 00:59:18 -0500

    Add link to source of info

--- home/.chezmoiscripts/run_onchange_install-zotero.sh
+++ home/.chezmoiscripts/run_onchange_install-zotero.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# More info at
+# https://github.com/retorquere/zotero-deb
 wget -qO- https://raw.githubusercontent.com/retorquere/zotero-deb/master/install.sh | sudo bash
 sudo apt update
 sudo apt install zotero
