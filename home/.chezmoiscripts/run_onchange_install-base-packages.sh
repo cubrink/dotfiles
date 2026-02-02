e91265a9b508f6fabbe628905caf22263198019b
Curtis Brinker <curtisbrnkr@gmail.com>
2026-02-01 23:23:28 -0600

    fix: support pacman for install scripts

--- home/.chezmoiscripts/run_onchange_install-base-packages.sh
+++ home/.chezmoiscripts/run_onchange_install-base-packages.sh
@@ -1,50 +1,106 @@
 #!/bin/sh
 set -e
 
-sudo apt update
+have() { command -v "$1" >/dev/null 2>&1; }
 
-sudo apt install \
-    build-essential \
-    make \
-    git \
-    curl \
-    vim \
-    htop \
-    wget \
-    tmux \
-    cmake \
-    sudo \
-    snap \
-    flatpak \
-    unzip \
-    7zip \
-    ffmpeg \
-    rsync \
-    ca-certificates \
-    gnupg \
-    tree \
-    jq \
-    vlc \
-    podman \
-    imagemagick \
-    pandoc \
-    golang \
-    ruby \
-    npm \
-    graphviz \
-    httpie \
-    gdb \
-    valgrind \
-    cmake-curses-gui \
-    cron \
-    unrar-free \
-    ghostscript \
-    shellcheck \
-    strace \
-    gh
+if have apt-get || have apt; then
+    sudo apt update
 
-    # taskwarrior \
-    # timewarrior \
-    # cargo \
-    # yq \
-    # tomlq \
+    sudo apt install -y \
+        build-essential \
+        make \
+        git \
+        curl \
+        vim \
+        htop \
+        wget \
+        tmux \
+        cmake \
+        sudo \
+        snap \
+        flatpak \
+        unzip \
+        7zip \
+        ffmpeg \
+        rsync \
+        ca-certificates \
+        gnupg \
+        tree \
+        jq \
+        vlc \
+        podman \
+        imagemagick \
+        pandoc \
+        golang \
+        ruby \
+        npm \
+        graphviz \
+        httpie \
+        gdb \
+        valgrind \
+        cmake-curses-gui \
+        cron \
+        unrar-free \
+        ghostscript \
+        shellcheck \
+        strace \
+        gh
+
+elif have pacman; then
+    # Update DB and system
+    sudo pacman -Sy --noconfirm
+    sudo yay -Sy --noconfirm
+
+    sudo pacman -S --needed --noconfirm \
+        base-devel \
+        make \
+        git \
+        curl \
+        vim \
+        htop \
+        wget \
+        tmux \
+        cmake \
+        sudo \
+        flatpak \
+        unzip \
+        p7zip \
+        ffmpeg \
+        rsync \
+        ca-certificates \
+        gnupg \
+        tree \
+        jq \
+        vlc \
+        podman \
+        imagemagick \
+        pandoc \
+        go \
+        ruby \
+        nodejs \
+        npm \
+        graphviz \
+        httpie \
+        gdb \
+        valgrind \
+        cmake \
+        cronie \
+        unrar \
+        ghostscript \
+        shellcheck \
+        github-cli \
+
+        # snapd \
+    sudo yay -S --needed --noconfirm \
+    	strace
+
+else
+    echo "No supported package manager found (apt or pacman). Aborting." >&2
+    exit 2
+fi
+
+# taskwarrior \
+# timewarrior \
+# cargo \
+# yq \
+# tomlq \
--- home/.chezmoiscripts/run_onchange_install-zotero.sh
+++ home/.chezmoiscripts/run_onchange_install-zotero.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # More info at
 # https://github.com/retorquere/zotero-deb
-wget -qO- https://raw.githubusercontent.com/retorquere/zotero-deb/master/install.sh | sudo bash
-sudo apt update
-sudo apt install zotero
+
+have() { command -v "$1" >/dev/null 2>&1; }
+
+if have apt-get || have apt; then
+	wget -qO- https://raw.githubusercontent.com/retorquere/zotero-deb/master/install.sh | sudo bash
+	sudo apt update
+	sudo apt install zotero
+elif have pacman; then
+	pacman -S --needed --noconfirm zotero
+else
+    echo "No supported package manager found (apt or pacman). Aborting." >&2
+    exit 2
+fi
