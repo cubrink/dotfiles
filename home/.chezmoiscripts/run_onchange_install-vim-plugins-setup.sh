0afdb7a78a84267dca1fe7e93f4dbaa34f09e6a4
Curtis Brinker <curtisbrnkr@gmail.com>
2026-05-03 16:30:35 -0500

    fix: ignore vim plugin auto install for now

--- home/.chezmoiscripts/run_onchange_install-vim-plugins-setup.sh
+++ home/.chezmoiscripts/run_onchange_install-vim-plugins-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-vim +qall # First pass - This installs plug.vim (via the .vimrc used)
-vim -c "PlugInstall" -c "qa!" # Second pass - Now that plug.vim is installed, we have the "PlugInstall" command
+# vim +qall # First pass - This installs plug.vim (via the .vimrc used)
+# vim -c "PlugInstall" -c "qa!" # Second pass - Now that plug.vim is installed, we have the "PlugInstall" command
